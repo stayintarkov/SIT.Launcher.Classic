efc3884a4b29a64f4c715ab7ba4be021f55349e3
PARGSoft <pargsoft@hotmail.com>
2022-05-29 21:44:08 +0100

    Added Auto Deobfuscate Assembly

---
+++ PaulovLauncher/LauncherConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulovLauncher
+{
+    public class LauncherConfig
+    {
+        public ICollection<ServerInstance> ServerInstances { get; set; }
+
+        public ServerInstance ServerInstance { get; set; }
+
+        public string Username { get; set; }
+
+        public bool AutomaticallyDeobfuscateDlls { get; set; }
+    }
+}
--- PaulovLauncher/MainWindow.xaml.cs
+++ PaulovLauncher/MainWindow.xaml.cs
@@ -7,7 +7,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.Loader;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,14 +32,21 @@ namespace PaulovLauncher
         {
             InitializeComponent();
 
-            if(File.Exists("LastUsedSettings.json"))
+            ExtractDeobfuscator();
+
+            if (File.Exists("LauncherConfig.json"))
             {
-                var ServerInstance = JsonConvert.DeserializeObject<ServerInstance>(File.ReadAllText("LastUsedSettings.json"));
-                ServerAddress = ServerInstance.ServerAddress;
+                Config = JsonConvert.DeserializeObject<LauncherConfig>(File.ReadAllText("LauncherConfig.json"));
             }
             this.DataContext = this;
 
         }
+
+        public LauncherConfig Config { get; set; } = new LauncherConfig() 
+        {
+            AutomaticallyDeobfuscateDlls = true,
+            ServerInstance = new ServerInstance() { ServerAddress = "https://localhost:443" }
+        };
 
         public IEnumerable<ServerInstance> ServerInstances 
         { 
@@ -47,15 +56,61 @@ namespace PaulovLauncher
             } 
         }
 
-        public string Username { get; set; }
 
-        private string _serverAddress = "https://localhost:443";
+        //private string _serverAddress = "https://localhost:443";
 
-        public string ServerAddress
+        public enum ELaunchButtonState
         {
-            get { return _serverAddress; }
-            set { _serverAddress = value; }
+            Launch,
+            Deob,
         }
+        public ELaunchButtonState LaunchButtonState { get; set; } = ELaunchButtonState.Launch;
+
+        private string _launchButtonText = "Launch";
+
+        public string LaunchButtonText 
+        { 
+            get 
+            {
+                switch (LaunchButtonState)
+                {
+                    case ELaunchButtonState.Launch:
+                        _launchButtonText = "Launch";
+                        break;
+                    case ELaunchButtonState.Deob:
+                        _launchButtonText = "Needs Deobfuscation";
+                        break;
+                    default:
+                        _launchButtonText = LaunchButtonState.ToString();
+                        break;
+                }
+                return _launchButtonText;
+            }
+            set
+            {
+                _launchButtonText = value;
+            }
+        }
+
+        //public string ServerAddress
+        //{
+        //    get { return _serverAddress; }
+        //    set { _serverAddress = value; }
+        //}
+
+        public string Username
+        {
+            get
+            {
+
+                return Config.Username;
+            }
+        }
+
+        public string ServerAddress { get {
+
+                return Config.ServerInstance.ServerAddress;
+            } }
 
 
         private void btnAddNewServer_Click(object sender, RoutedEventArgs e)
@@ -69,7 +124,7 @@ namespace PaulovLauncher
 
         }
 
-        private void btnLaunchGame_Click(object sender, RoutedEventArgs e)
+        private async void btnLaunchGame_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(ServerAddress))
             {
@@ -117,12 +172,20 @@ namespace PaulovLauncher
                 openFileDialog.Filter = "Executable (EscapeFromTarkov.exe)|EscapeFromTarkov.exe;";
                 if(openFileDialog.ShowDialog() == true)
                 {
-                    var commandArgs = $"-token={returnData} -config={{\"BackendUrl\":\"{ServerAddress}\",\"Version\":\"live\"}}";
-                    Process.Start(openFileDialog.FileName, commandArgs);
-
-                    File.WriteAllText("LastUsedSettings.json", JsonConvert.SerializeObject(new ServerInstance() { ServerAddress = ServerAddress }));
-
-                    WindowState = WindowState.Minimized;
+                    // Deobfuscate Assembly-CSharp
+                    if (Config.AutomaticallyDeobfuscateDlls 
+                        && NeedsDeobfuscation(openFileDialog.FileName))
+                    {
+                        if(await Deobfuscate(openFileDialog.FileName))
+                        {
+                            StartGame(returnData, openFileDialog);
+                        }
+                    }
+                    else
+                    {
+                        // Launch game
+                        StartGame(returnData, openFileDialog);
+                    }
                 }
             }
             else if (returnData == "ALREADY_IN_USE")
@@ -132,6 +195,130 @@ namespace PaulovLauncher
             else
             {
                 var messageBoxResult = MessageBox.Show("Something went wrong.", "Account");
+            }
+        }
+
+        private void StartGame(string sessionId, OpenFileDialog openFileDialog)
+        {
+            var battlEyeDirPath = Directory.GetParent(openFileDialog.FileName).FullName + "\\BattlEye";
+            if (Directory.Exists(battlEyeDirPath))
+            {
+                Directory.Delete(battlEyeDirPath, true);
+            }
+            var battlEyeExePath = openFileDialog.FileName.Replace("EscapeFromTarkov", "EscapeFromTarkov_BE");
+            if (File.Exists(battlEyeExePath))
+            {
+                File.Delete(battlEyeExePath);
+            }
+
+            var commandArgs = $"-token={sessionId} -config={{\"BackendUrl\":\"{ServerAddress}\",\"Version\":\"live\"}}";
+            Process.Start(openFileDialog.FileName, commandArgs);
+            File.WriteAllText("LauncherConfig.json", JsonConvert.SerializeObject(Config));
+            WindowState = WindowState.Minimized;
+        }
+
+        private async Task<bool> Deobfuscate(string exeLocation)
+        {
+            var deobfusFolder = System.AppContext.BaseDirectory + "/DeObfus/";
+            var deobfusApp = deobfusFolder + "de4dot-x64.exe";
+
+            // Discover where Assembly-CSharp is within the Game Folders
+            var assemblyLocation = exeLocation.Replace("EscapeFromTarkov.exe", "");
+            assemblyLocation += "EscapeFromTarkov_Data\\Managed\\Assembly-CSharp.dll";
+
+            // Backup the Assembly-CSharp and place the newest clean one
+            if (!File.Exists(assemblyLocation + ".backup"))
+            {
+                File.Copy(assemblyLocation, assemblyLocation + ".backup");
+
+                List<FileInfo> fileInfos = Directory.GetFiles(AppContext.BaseDirectory + "/DeObfus/PatchedAssemblies/").Select(x => new FileInfo(x)).ToList();
+                var lastAssembly = fileInfos.OrderByDescending(x => x.LastWriteTime).FirstOrDefault();
+                lastAssembly.CopyTo(assemblyLocation, true);
+            }
+            return true;
+
+            //// Extract the Deobfuscator zip
+            //ExtractDeobfuscator();
+
+            //// Discover if it needs Deobfuscation
+            ////if (NeedsDeobfuscation(exeLocation))
+            ////{
+            //    // Delete any left over Assembly-CSharp.dll file
+            //    if(File.Exists("Assembly-CSharp.dll"))
+            //        File.Delete("Assembly-CSharp.dll");
+
+            //    // Backup the Assembly-Csharp
+            //    File.Copy(assemblyLocation, assemblyLocation + ".backup", true);
+            //    // Copy the Assembly-Csharp to our folder
+            //    File.Copy(assemblyLocation, "Assembly-CSharp.dll", true);
+
+            //    var commandArgs = "--un-name \"!^<>[a-z0-9]$&!^<>[a-z0-9]__.*$&![A-Z][A-Z]\\$<>.*$&^[a-zA-Z_<{$][a-zA-Z_0-9<>{}$.`-]*$\" \"Assembly-CSharp.dll\"";
+            //    LaunchButtonState = ELaunchButtonState.Deob;
+            //    btnLaunchGame.IsEnabled = false;
+            //    var deobfusProcess = Process.Start(deobfusApp, commandArgs);
+            //    if (deobfusProcess != null)
+            //    {
+            //        await Task.Run(() =>
+            //        {
+            //            while (!deobfusProcess.HasExited) { }
+            //        });
+            //        LaunchButtonState = ELaunchButtonState.Launch;
+            //        btnLaunchGame.IsEnabled = true;
+            //        File.Copy("Assembly-CSharp-cleaned.dll", assemblyLocation, true);
+
+            //        return true;
+            //    }
+            ////}
+            //return false;
+        }
+
+        private bool NeedsDeobfuscation(string exeLocation)
+        {
+            var assemblyLocation = exeLocation.Replace("EscapeFromTarkov.exe", "");
+            assemblyLocation += "EscapeFromTarkov_Data\\Managed\\Assembly-CSharp.dll";
+            return !File.Exists(assemblyLocation + ".backup");
+                //var deobfusFolder = System.AppContext.BaseDirectory + "/DeObfus/";
+                //var deobfusApp = deobfusFolder + "de4dot-x64.exe";
+                //AssemblyLoadContext loadContext = new AssemblyLoadContext("AssemblyCSharpLC",true);
+
+                //var assemblyLocation = exeLocation.Replace("EscapeFromTarkov.exe", "");
+                //assemblyLocation += "EscapeFromTarkov_Data\\Managed\\Assembly-CSharp.dll";
+                //File.Copy(assemblyLocation, "Assembly-CSharp-Readable.dll", true);
+
+                ////var assembly = loadContext.LoadFromAssemblyPath( AppContext.BaseDirectory + "\\Assembly-CSharp-Readable.dll");
+                //var assembly = System.Reflection.Assembly.LoadFrom("Assembly-CSharp-Readable.dll");
+                //if (assembly == null)
+                //    throw new DllNotFoundException("Assembly-CSharp.dll cannot be found");
+
+                //try
+                //{
+                //    var type = assembly.GetTypes().FirstOrDefault(t => t.Name == "AbstractGame");
+                //    if (type == null)
+                //        throw new DllNotFoundException("Could not find AbstractGame in Assembly-CSharp.dll");
+
+                //    var methodToCheck = type.GetMethod("\\uE000", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                //    return (methodToCheck != null);
+                //}
+                //catch
+                //{
+
+                //}
+
+                //loadContext.Unload();
+                //Thread.Sleep(2000);
+                //return true;
+        }
+
+        private void ExtractDeobfuscator()
+        {
+            var deobfusFolder = System.AppContext.BaseDirectory + "/DeObfus/";
+            if (!File.Exists(deobfusFolder + "Deobfuscator.zip"))
+                return;
+
+            if (!File.Exists(deobfusFolder + "de4dot-x64.exe"))
+            {
+                System.IO.Compression.ZipFile.ExtractToDirectory(deobfusFolder + "Deobfuscator.zip", deobfusFolder);
+                File.Delete(deobfusFolder + "Deobfuscator.zip");
             }
         }
     }
