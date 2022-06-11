b2969e67ee72dfe550874a72a4b4c2ac2516299f
PARGSoft <pargsoft@hotmail.com>
2022-06-11 23:59:15 +0100

    New settings screen

---
+++ PaulovLauncher/Windows/Settings.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MahApps.Metro.Controls;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIT.Launcher.Windows
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : MetroWindow
+    {
+        public Settings()
+        {
+            InitializeComponent();
+
+            if (File.Exists("LauncherConfig.json"))
+            {
+                Config = JsonConvert.DeserializeObject<LauncherConfig>(File.ReadAllText("LauncherConfig.json"));
+            }
+            this.DataContext = this;
+            
+        }
+
+        public LauncherConfig Config { get; } = LauncherConfig.Instance;
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Config.Save();
+            new MainWindow().Show();
+            base.OnClosing(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+        }
+    }
+}
